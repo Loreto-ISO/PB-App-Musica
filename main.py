94fd94c0bf0916c5dff8279c5e4967a709cdc2c2
Loreto Estefany Millanao Ubeda <loreto.millanao@inacapmail.cl>
2026-09-03 22:37:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -44,3 +44,55 @@ def main():
 
 if __name__=="__main__":
     main()
+
+
+
+
+
+
+
+
+
+    # # CREAR CANCION
+    # cancion_uno = Cancion("Beat it",4.50, "Pop")
+    # cancion_dos = Cancion("Bad", 3.25, "Pop")
+
+    # # CREAR Podcast
+    # podcast_uno = Podcast("Ultima luna", 35, "Comedia", 15)
+
+    # #CREAR ARTISTA
+
+    # nuevo_artista = Artista("Michael Jackson", "Pop")
+
+    # #Asociar las canciones al artista
+    # nuevo_artista.agregar_cancion(cancion_uno)
+    # nuevo_artista.agregar_cancion(cancion_dos)
+
+    # nuevo_artista.mostrar_informacion()
+
+    # #CREAR PLAYLIST
+    # nueva_playlist = Playlist("Antiguas pero bonitas", "Las mejores canciones del mundo")
+    # # nueva_playlist.mostrar_playlist()
+
+    # nueva_playlist.agregar_cancion(cancion_uno)
+    # nueva_playlist.mostrar_playlist()
+
+    # #CREAR USUARIO
+
+    # new_user = Usuario("Juan Perez", "jp@gmail.com", True)
+
+    # #ASOCIAR LA PLAYLIST AL USUARIO
+
+    # new_user.crear_playlist(nueva_playlist)
+
+    # new_user.mostrar_informacion()
+
+    # # CREAR ALBUM
+
+    # album1 = Album("Prime",2001)
+
+    # #agregar canciones
+
+    # album1.agregar_cancion(cancion_dos)
+
+    # album1.mostrar_album()
